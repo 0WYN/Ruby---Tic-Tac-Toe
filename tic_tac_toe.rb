d54230ba4fff1791bbd9a2c80937a075999d4dd3
Adam Halfpenny <adamhalfpenny@gmail.com>
2020-04-16 12:52:16 -0400

    completed basic functionality

--- tic_tac_toe.rb
+++ tic_tac_toe.rb
@@ -1,25 +1,24 @@
 class Game
     @@gameboard = Array.new(9, " ")
-    @@is_winner = false
+    @@rounds = 0
 
-    def initialize(first_player)
-        @first_player = first_player
+    def initialize
+        @first_player = "x"
+        @second_player = "o"
+        self.play 
     end
 
     def winner?
         winning_combos = [[0,1,2], [3,4,5], [6,7,8], [0,3,6], [1,4,7], [2,5,8], [0,4,8], [6,4,2]]
-        #0,1,2 - top row
-        #3,4,5 - middle row
-        #6,7,8 - bottom row
-        #0,3,6 - left column
-        #1,4,7 - middle column
-        #2,5,8 - right column
-        #0,4,8 - back slash
-        #6,4,2 - forward slash
+        @@rounds += 1
         winning_combos.each do |combo|
             if @@gameboard[combo[0]] == @@gameboard[combo[1]] && @@gameboard[combo[1]] == @@gameboard[combo[2]] && !@@gameboard[combo[2]].strip.empty?
                 puts "WINNER!"
                 puts combo.join(",")
+                exit
+            elsif @@rounds == 9
+                puts "DRAW!"
+                exit
             end
         end
     end
@@ -30,26 +29,41 @@ class Game
              "#{@@gameboard[3]} | #{@@gameboard[4]} | #{@@gameboard[5]}\n" \
              "--+---+--\n" \
              "#{@@gameboard[6]} | #{@@gameboard[7]} | #{@@gameboard[8]}"
+
+             self.winner?
     end
 
     def change_board!(location, value)
         @@gameboard[location] = value
     end
 
-    def to_s
-    if self.nil?
-        print "jkfslj;adskl"
-    else super
+    def play
+
+        while @@gameboard.include?(" ") do
+            puts "Player One please pick your location for your '#{@first_player}' (1-9)"
+            @@gameboard[self.get_input] = @first_player
+            self.print_board
+            puts "Player Two please pick your location for your '#{@second_player}' (1-9)"
+            @@gameboard[self.get_input] = @second_player
+            self.print_board
+        end
     end
+
+    def get_input
+        valid_range = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+        input = gets.chomp.to_i
+        if valid_range.include?(input) && @@gameboard[input - 1].strip.empty?
+            return input - 1
+        else 
+            puts "Please enter a valid number"
+            self.get_input
+        end
+    
     end
+
 end
 
+game = Game.new
 
-game = Game.new("x")
-#game.print_board
-#game.change_board!(0, "x")
-#game.change_board!(3, "x")
-game.change_board!(6, "x")
-game.print_board
-game.winner?
+
 
